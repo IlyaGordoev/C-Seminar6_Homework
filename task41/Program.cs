7fa48445571c558a3dd98b99bdb4679c259e90e6
Ilya <gordoev@yandex.ru>
2023-05-28 15:06:33 +0300

    add all

---
+++ task41/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел
+// больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
+
+int[] FillArray(int length)
+{
+int[] array = new int[length];
+for (int i = 0; i < length; i++)
+{
+    System.Console.WriteLine($"Введите число №{i+1}: ");
+    array[i] = Convert.ToInt32(Console.ReadLine()); 
+}
+System.Console.WriteLine(string.Join(", ", array));
+return array;
+}
+
+void CountPositiveNum(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    System.Console.WriteLine(count);
+}
+
+System.Console.Write("Сколько чисел хотите ввести: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArray(m);
+CountPositiveNum(array);
---
+++ task43/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// k1 = 5,  b1 = 2, k2 = 9, b2 = 4 -> (-0,5; -0,5)
+
+double[] CrossPoint( double k1, double b1, double k2, double b2)
+{
+    double[] array = new double[2];
+    array[0] = (b2-b1)/(k1-k2);
+    array[1] = k1 * array[0] + b1;
+
+    System.Console.Write($"Точка пересечения: ({string.Join(", ", array)})");
+    return array;
+}
+
+System.Console.WriteLine("Точка пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2");
+
+System.Console.WriteLine("Введите k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+
+System.Console.WriteLine("Введите b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+
+System.Console.WriteLine("Введите k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+System.Console.WriteLine("Введите b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+CrossPoint(k1, b1, k2, b2);
